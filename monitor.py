a5b9e46130c4771e0a2faece8c0edd43c6f8df5f
Riyas812 <mohamedriyasm1700@gmail.com>
2025-09-12 17:46:42 +0530

    Update monitor1

--- monitor.py
+++ monitor.py
@@ -1,37 +1,72 @@
-
 from time import sleep
 import sys
 
+# Global language variable: 'en' for English, 'de' for German
+LANGUAGE = 'en'
 
-def vitals_ok(temperature, pulseRate, spo2):
-  if temperature > 102 or temperature < 95:
-    print('Temperature critical!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  elif pulseRate < 60 or pulseRate > 100:
-    print('Pulse Rate is out of range!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  elif spo2 < 90:
-    print('Oxygen Saturation out of range!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  return True
+# Dictionary for localized messages
+MESSAGES = {
+    'en': {
+        'temperature': 'Temperature critical!',
+        'pulseRate': 'Pulse Rate is out of range!',
+        'spo2': 'Oxygen Saturation out of range!',
+        'blood_sugar': 'Blood Sugar out of range!',
+        'blood_pressure': 'Blood Pressure out of range!',
+        'respiratory_rate': 'Respiratory Rate out of range!',
+    },
+    'de': {
+        'temperature': 'Temperatur kritisch!',
+        'pulseRate': 'Pulsrate außerhalb des Bereichs!',
+        'spo2': 'Sauerstoffsättigung außerhalb des Bereichs!',
+        'blood_sugar': 'Blutzucker außerhalb des Bereichs!',
+        'blood_pressure': 'Blutdruck außerhalb des Bereichs!',
+        'respiratory_rate': 'Atemfrequenz außerhalb des Bereichs!',
+    }
+}
+
+# Vital parameters and their limits
+VITALS_LIMITS = {
+    'temperature': (95, 102),
+    'pulseRate': (60, 100),
+    'spo2': (90, 100),  # Assuming 100 is max SPO2
+    'blood_sugar': (70, 110),
+    'blood_pressure': (90, 150),
+    'respiratory_rate': (12, 20)
+}
+
+def blink_alert():
+    """Blinking alert animation."""
+    for _ in range(6):
+        print('\r* ', end='')
+        sys.stdout.flush()
+        sleep(1)
+        print('\r *', end='')
+        sys.stdout.flush()
+        sleep(1)
+    print()  # Move to the next line after blinking
+
+def check_vital(name, value):
+    """Check a single vital sign value against limits."""
+    low, high = VITALS_LIMITS[name]
+    if not (low <= value <= high):
+        # Print localized message
+        print(MESSAGES[LANGUAGE][name])
+        blink_alert()
+        return False
+    return True
+
+def vitals_ok(temperature, pulseRate, spo2, blood_sugar, blood_pressure, respiratory_rate):
+    """Check all vitals, return True only if all are OK."""
+    vitals_values = {
+        'temperature': temperature,
+        'pulseRate': pulseRate,
+        'spo2': spo2,
+        'blood_sugar': blood_sugar,
+        'blood_pressure': blood_pressure,
+        'respiratory_rate': respiratory_rate
+    }
+
+    for vital_name, vital_value in vitals_values.items():
+        if not check_vital(vital_name, vital_value):
+            return False
+    return True
